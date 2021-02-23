ddd9c79b7b5a133ec33e9a53124ba6b6f3163c0b
郑超 <1036717543@qq.com>
2021-02-23 17:35:46 +0800

    222

--- Caching.Memory.Application/CacheEntryHelper.cs
+++ Caching.Memory.Application/CacheEntryHelper.cs
@@ -6,6 +6,41 @@ namespace Caching.Memory.Application
 {
     public class CacheEntryHelper
     {
+        internal static IDisposable EnterScope(CacheEntry entry)
+        {
+            var scopes = GetOrCreateScopes();
 
+            var scopeLease = new ScopeLease(scopes);
+            Scopes = scopes.Push(entry);
+
+            return scopeLease;
+        }
+
+        private static CacheEntryStack GetOrCreateScopes()
+        {
+            var scopes = Scopes;
+            if (scopes == null)
+            {
+                scopes = CacheEntryStack.Empty;
+                Scopes = scopes;
+            }
+
+            return scopes;
+        }
+
+        private sealed class ScopeLease : IDisposable
+        {
+            readonly CacheEntryStack _cacheEntryStack;
+
+            public ScopeLease(CacheEntryStack cacheEntryStack)
+            {
+                _cacheEntryStack = cacheEntryStack;
+            }
+
+            public void Dispose()
+            {
+                Scopes = _cacheEntryStack;
+            }
+        }
     }
 }
